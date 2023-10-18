4df84951f79887b887901bd030e8d325c5914d7f
Sibaji Khillar <sibajikhillar@gmail.com>
2023-10-18 12:28:44 +0530

    15/10/2023

--- Bit_Manupulation/Operations.java
+++ Bit_Manupulation/Operations.java
@@ -24,9 +24,20 @@ public class Operations {
         return (n & bitMask);
     }
 
+    // update ith bit
+    public static int updateIthBit(int n, int i, int newBit){
+        if(newBit == 0){
+            return clearIthBit(n, i);
+        }
+        else{
+            return setIthBit(n, i);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(getIthBit(10, 3));   //set ith bit
         System.out.println(setIthBit(10, 2));    //get ith bit
         System.out.println(clearIthBit(10, 1));    // clear ith bit
+        System.out.println(updateIthBit(10, 2, 1)); //update ith bit
     }
 }
