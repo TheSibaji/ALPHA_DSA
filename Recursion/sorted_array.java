d0ac619c07fa3840f68f0ccf47ec8057b8339eb6
Sibaji Khillar <sibajikhillar@gmail.com>
2023-12-17 21:14:22 +0530

    15,16,17/Dec/2023

---
+++ Recursion/sorted_array.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class sorted_array {
+
+    // check idf a array is sorted or not
+    public static boolean isSorted(int arr[], int i){
+        // base case
+        if(i == arr.length-1){
+            return true;
+        }
+        
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 4, 6};
+        int arr2[] = {21, 6, 8, 4, 90};
+
+        System.out.println(isSorted(arr, 0));
+        System.out.println(isSorted(arr2, 0));
+    }
+}
